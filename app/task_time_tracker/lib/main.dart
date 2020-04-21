24dc7abc55b5750d03fc357dff8c27d03d55e50e
Darryn Jacobs <djacobs900@gmail.com>
2020-04-21 22:06:00 +0200

    --wip-- [skipci] - Added the home page bottom nav - Added nav widgets

--- app/task_time_tracker/lib/main.dart
+++ app/task_time_tracker/lib/main.dart
@@ -6,10 +6,96 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      )
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  final navigationBarTabs = [
+    HomeWidget(),
+    WeekWidget(),
+    TasksWidget(),
+    SettingsWidget()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationBarTabs[this._currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: this._currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+            backgroundColor: Colors.blueAccent,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            title: Text("Week"),
+            backgroundColor: Colors.redAccent,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check_circle_outline),
+            title: Text("Tasks"),
+            backgroundColor: Colors.greenAccent,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text("Settings"),
+            backgroundColor: Colors.grey,
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            this._currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Home page selected"),
+    );
+  }
+}
+
+class WeekWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Week page selected"),
+    );
+  }
+}
+
+class TasksWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Tasks page selected"),
+    );
+  }
+}
+
+class SettingsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Settings page selected"),
     );
   }
 }
