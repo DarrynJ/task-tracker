88eb58f6be27207decc58ab87a470ae371500c76
Darryn Jacobs <djacobs900@gmail.com>
2020-04-22 14:07:30 +0200

    --wip-- [skipci] - Updated the navbar styling.

--- app/task_time_tracker/lib/main.dart
+++ app/task_time_tracker/lib/main.dart
@@ -30,36 +30,7 @@ class HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: navigationBarTabs[this._currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: this._currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text("Home"),
-            backgroundColor: Colors.blueAccent,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            title: Text("Week"),
-            backgroundColor: Colors.redAccent,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.check_circle_outline),
-            title: Text("Tasks"),
-            backgroundColor: Colors.greenAccent,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            title: Text("Settings"),
-            backgroundColor: Colors.grey,
-          ),
-        ],
-        onTap: (index) {
-          setState(() {
-            this._currentIndex = index;
-          });
-        },
-      ),
+      bottomNavigationBar: BottomNavigationWidget(),
     );
   }
 }
@@ -96,6 +67,85 @@ class SettingsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Text("Settings page selected"),
+    );
+  }
+}
+
+class BottomNavigationWidget extends StatefulWidget {
+  @override
+  _BottomNavigationWidgetState createState() => _BottomNavigationWidgetState();
+}
+
+class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
+  int _currentIndex = 0;
+
+  final navigationBarTabs = [
+    HomeWidget(),
+    WeekWidget(),
+    TasksWidget(),
+    SettingsWidget()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationBarTabs[this._currentIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(30),
+            topLeft: Radius.circular(30),
+            bottomLeft: Radius.circular(30.0),
+            bottomRight: Radius.circular(30.0),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              spreadRadius: 0,
+              blurRadius: 25,
+            ),
+          ],
+        ),
+        padding: const EdgeInsets.all(10),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+            bottomLeft: Radius.circular(30.0),
+            bottomRight: Radius.circular(30.0),
+          ),
+          child: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text("Home"),
+                backgroundColor: Colors.blueAccent,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today),
+                title: Text("Week"),
+                backgroundColor: Colors.redAccent,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.check_circle_outline),
+                title: Text("Tasks"),
+                backgroundColor: Colors.greenAccent,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                title: Text("Settings"),
+                backgroundColor: Colors.grey,
+              ),
+            ],
+            onTap: (index) {
+              setState(() {
+                this._currentIndex = index;
+              });
+            },
+          ),
+        ),
+      ),
     );
   }
 }
