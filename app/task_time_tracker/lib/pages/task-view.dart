1aac96d1dc025b90695b494c292c625c86eccb50
Darryn Jacobs <darrynjacobs@Darryns-MacBook-Pro.local>
2020-05-06 21:30:38 +0200

    --wip--[skipci] - Initial update to the task-view page

--- app/task_time_tracker/lib/pages/task-view.dart
+++ app/task_time_tracker/lib/pages/task-view.dart
@@ -17,22 +17,104 @@ class _TaskViewState extends State<TaskView> {
 
   @override
   Widget build(BuildContext context) {
+    String _title = "";
+    String _description = "";
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(this.task.name),
-        actions: <Widget>[
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.person_outline),
-                tooltip: 'Profile',
-                onPressed: () {},
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(110, 111, 190, 1),
+        ),
+        child: Column(
+          children: <Widget>[
+            // Overview of task entries over the month
+            SafeArea(
+              child: Container(
+                height: 275,
+                margin: EdgeInsets.only(
+                  bottom: 10,
+                  left: 15,
+                  right: 15,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(50),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black87,
+                      blurRadius:
+                          10.0, // has the effect of softening the shadow
+                      spreadRadius:
+                          0.0, // has the effect of extending the shadow
+                      offset: Offset(
+                        0.0, // horizontal, move right 10
+                        1.5, // vertical, move down 10
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ],
-          )
-        ],
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(
+                  top: 25,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black87,
+                      blurRadius:
+                          10.0, // has the effect of softening the shadow
+                      spreadRadius:
+                          0.0, // has the effect of extending the shadow
+                      offset: Offset(
+                        0.0, // horizontal, move right 10
+                        1.5, // vertical, move down 10
+                      ),
+                    )
+                  ],
+                ),
+                child: SafeArea(
+                  child: Form(
+                    child: Container(
+                      padding: EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            decoration: InputDecoration(hintText: "Title"),
+                            autofocus: true,
+                            onSaved: (value) => _title = value,
+                            validator: (value) => value.length == 0
+                                ? "A title must be supplied."
+                                : null,
+                          ),
+                          TextFormField(
+                            decoration:
+                                InputDecoration(hintText: "Description"),
+                            minLines: 3,
+                            maxLines: 5,
+                            onSaved: (value) => _description = value,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
