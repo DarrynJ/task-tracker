df5cb7a340a43d97b3464d32b9ac143555509dcb
Darryn Jacobs <darrynjacobs@Darryns-MacBook-Pro.local>
2020-05-10 22:58:10 +0200

    --wip-- [skipci]

--- app/task_time_tracker/lib/models/task.dart
+++ app/task_time_tracker/lib/models/task.dart
@@ -3,6 +3,26 @@ class Task {
   String name;
   String description;
   bool taskStarted = false;
+  int seconds = 0;
 
   Task(this.id, this.name, this.description, this.taskStarted);
+
+  Duration getDuration() {
+    return new Duration(seconds: this.seconds);
+  }
+
+  String toCapturedDisplayTime() {
+    Duration capturedDuration = this.getDuration();
+
+    if (capturedDuration.inHours > 0)
+      return "${capturedDuration.inHours.toString()} hrs";
+    else if (capturedDuration.inMinutes > 0)
+      return "${capturedDuration.inMinutes.toString()} min";
+    else if (capturedDuration.inSeconds > 0)
+      return "${capturedDuration.inSeconds.toString()} sec";
+    else
+      return "";
+  }
+
+  String format(Duration d) => d.toString().split('.').first.padLeft(8, "0");
 }
